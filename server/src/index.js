2cf4402c75a572494673b03bf847dbd9573812ba
uthdev <adelekegbolahan92@yahoo.com>
2018-12-31 11:35:44 +0100

    set up the server side

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // set port for server to listen on
-const Port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 // support parsing of application/json type post data
 app.use(express.json());
