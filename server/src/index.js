7f6aa554aec881e0e37f1c2257d1253d04cedd0e
uthdev <adelekegbolahan92@yahoo.com>
2019-01-01 09:30:25 +0100

    add a meetup dummy database object

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-// set port for server to listen on
-const port = process.env.PORT || 5000;
-
 // support parsing of application/json type post data
 app.use(express.json());
 
