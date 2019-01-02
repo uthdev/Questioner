216bc5fab7339db64607bea38b2cd0126dec848f
uthdev <adelekegbolahan92@yahoo.com>
2019-01-02 15:59:39 +0100

    feature api endpoint: build a POST api/v1/questions endpoint

---
+++ server/src/data/questions.js
@@ -0,0 +1,49 @@
+const questionsDb = [
+  {
+    id: 1,
+    createdOn: new Date('02-03-2019'),
+    createdBy: 3,
+    meetup: 3,
+    title: 'Naruto and Sasuke',
+    body: 'Who is stronger between Naruto and Sasuke?',
+    votes: 5
+  },
+  {
+    id: 2,
+    createdOn: new Date('02-05-2019'),
+    createdBy: 2,
+    meetup: 1,
+    title: 'Jose Mourihno and Man U',
+    body: 'Was sacking mourihno the solution to Man U problem?',
+    votes: 19
+  },
+  {
+    id: 3,
+    createdOn: new Date('02-10-2019'),
+    createdBy: 5,
+    meetup: 3,
+    title: 'Hokage: Tobirama, Minato and Naruto',
+    body: 'Who is the fastest of all the Hokages?',
+    votes: 20
+  },
+  {
+    id: 4,
+    createdOn: new Date('03-01-2019'),
+    createdBy: 1,
+    meetup: 2,
+    title: 'Marvel and DC',
+    body: 'Who produced the best Superhero Movie of 2018?',
+    votes: 15
+  },
+  {
+    id: 5,
+    createdOn: new Date('01-20-2019'),
+    createdBy: 4,
+    meetup: 1,
+    title: 'Premier league 2018/2019',
+    body: 'Can Liverpool really win the premier league this season?',
+    votes: 12
+  }
+];
+
+export default questionsDb;
