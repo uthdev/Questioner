6d4708fb731537372ffeaac4b9b6b8e32a33697c
uthdev <adelekegbolahan92@yahoo.com>
2019-01-03 19:50:54 +0100

    add POST rsvps to meetup route

--- server/src/routes/meetups/meetupRoutes.js
+++ server/src/routes/meetups/meetupRoutes.js
@@ -7,6 +7,7 @@ const meetupRoute = express();
 meetupRoute.route('/').get(meetupController.allMeetup);
 meetupRoute.route('/upcoming').get(meetupController.upcomingMeetup);
 meetupRoute.route('/:id').get(meetupController.getAMeetup);
+meetupRoute.route('/:id/rsvps').post(meetupController.rsvpMeetup);
 meetupRoute.route('/').post(meetupController.createMeetup);
 
 export default meetupRoute;
