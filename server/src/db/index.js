b94e66bf058f3a8b007840d1cbbf55378d6aadc0
uthdev <adelekegbolahan92@yahoo.com>
2019-01-12 19:37:52 +0100

    [Chore #163166448] set up database

---
+++ server/src/db/index.js
@@ -0,0 +1,24 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+dbConfig.test = {
+  user: process.env.TEST_DB_USER,
+  host: process.env.TEST_DB_HOST,
+  database: process.env.TEST_DB_NAME,
+  password: process.env.TEST_DB_PASSWORD,
+  port: process.env.TEST_DB_PORT,
+};
+
+dbConfig.database = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
+
+const dbPool = (process.env.NODE_ENV === 'production') ? new Pool(dbConfig.database) : new Pool(dbConfig.test);
+
+export default dbPool;
