6d8666bedb11cd9ab9cf03ad8654e529d8357369
uthdev <adelekegbolahan92@yahoo.com>
2019-01-16 02:57:03 +0100

    [Feature #162681773] reconfigured the database

---
+++ server/src/config/index.js
@@ -0,0 +1,18 @@
+const { Pool } = require('pg')
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log(process.env.DATABASE_URL);
+
+const pool = new Pool({ 
+  connectionString : process.env.DATABASE_URL,
+})
+
+module.exports = {
+  query: (text, params, callback) => {
+    const start = Date.now();
+    console.log(start)
+    return pool.query(text, params, callback);
+    }
+}
